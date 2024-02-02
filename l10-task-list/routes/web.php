2168eceac27c8a0f5708fdec9e0c95627391481e
Uriel Paez <carlos.paez@afirma-solutions.com>
2024-02-02 16:41:54 -0600

    "feat: Se agrega el prefijo tasks a las rutas de index y show. Se busca el objeto con el ID enviado en la lista de tareas"

--- l10-task-list/routes/web.php
+++ l10-task-list/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Symfony\Component\HttpFoundation\Response;
 
 /*
 |--------------------------------------------------------------------------
@@ -68,12 +69,20 @@ $tasks = [
     ),
 ];
 
-Route::get('/', function () use ($tasks) {
+Route::get('/', function () {
+    return redirect()->route('task.index');
+});
+
+Route::get('/tasks', function () use ($tasks) {
     return view('index', [
         'tasks' => $tasks
     ]);
 })->name('task.index');
 
-Route::get('/{id}', function ($id) {
-    return 'One single task';
+Route::get('/tasks/{id}', function ($id) use ($tasks) {
+    $task = collect($tasks)->firstWhere('id', $id);
+    if (!$task) {
+        abort(Response::HTTP_NOT_FOUND);
+    }
+    return view('show', ['task' => $task]);
 })->name('task.show');
