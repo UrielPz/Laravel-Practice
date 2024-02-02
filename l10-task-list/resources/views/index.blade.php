8a0728d5defe80fa9583a2376913610b8ca4a0eb
Uriel Paez <carlos.paez@afirma-solutions.com>
2024-02-02 16:43:55 -0600

    "feat: Se usa la plantilla de layout en la vista de index"

--- l10-task-list/resources/views/index.blade.php
+++ l10-task-list/resources/views/index.blade.php
@@ -1,11 +1,17 @@
-<div>
-    @forelse($tasks as $task)
-        <div>
-            <a href="{{ route('task.show', ['id' => $task->id]) }}">{{ $task->title }}</a>
-        </div>
-    @empty
-        <div>
-            There are not tasks!
-        </div>
-    @endforelse
-</div>
+@extends('layouts.app')
+
+@section('title', 'The list of tasks')
+
+@section('content')
+    <div>
+        @forelse($tasks as $task)
+            <div>
+                <a href="{{ route('task.show', ['id' => $task->id]) }}">{{ $task->title }}</a>
+            </div>
+        @empty
+            <div>
+                There are not tasks!
+            </div>
+        @endforelse
+    </div>
+@endsection
